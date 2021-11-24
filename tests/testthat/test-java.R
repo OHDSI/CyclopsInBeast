86a754fcfb22dec720cb7433a363c41c57fa8e37
Marc Suchard <msuchard@ucla.edu>
2021-11-24 09:26:06 -0800

    ant build works

--- tests/testthat/test-java.R
+++ tests/testthat/test-java.R
@@ -1,3 +1,10 @@
+library(testthat)
+
 test_that("Java support", {
   skip_if_not(supportsJava8())
+})
+
+test_that("Find BEAST JAR", {
+  fileName <- system.file("java/beast.jar", package = "BeastJar")
+  expect_true(file.exists(fileName))
 })
